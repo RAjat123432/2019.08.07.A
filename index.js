bb2bee2c855f30ef0aa25be34c54d4e953c92000
Rajat Parbhakar <749084@student.tor.loc>
2019-08-07 17:20:10 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extended}));
+app.use(bodyparser.json());
+app.use(function(req,res,next){
+    console.log("I am in the Middle");
+    console.log(req.body.username);
+    console.log(req.body.password);
+    next();
+})
+
 app.get('/', function(req,res){
-    res.send("<h1>Hello World2")
-});
+    res.send('GET sent')
+})
 app.post('/', function(req,res){
-res.send("Hello World")
-}
-)
+    res.send('POST sent')
+})
+app.put('/', function(req,res){
+    res.send('PUT sent ')
+})
+app.delete('/', function(req,res){
+    res.send('DELETE sent')
+})
+
 app.listen(3000);
